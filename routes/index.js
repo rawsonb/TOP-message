cbcf8218f1b7e2c360c4e15eb99a3d4895d7d83b
f.blackmon <f.rawsonblackmon@gmail.com>
2022-12-04 12:18:47 -0500

    initial working ver

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render("index", {messages: messages});
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'Express' });
+});
+
+router.post("/new", (req, res, next) => {
+  messages.push({
+    text:   req.body.postText,
+    user: req.ip,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
